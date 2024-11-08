824a68e650d192494a265fbf90855ad315bc77f8
Rijjwal21 <rigsarora@gmail.com>
2024-11-08 22:57:36 +0530

    first commit

---
+++ knowledge-retrieval-engine-main/gemini.py
@@ -0,0 +1,42 @@
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+import PIL.Image
+from PyPDF2 import PdfReader
+# from IPython.display import Markdown
+
+load_dotenv()
+GEMINI_KEY = os.getenv('gemini_key')
+
+genai.configure(api_key = GEMINI_KEY)
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+# Extracting text from pdf
+def extract_text(file):
+    reader = PdfReader(file)
+    file_text = ""
+    for page in reader.pages:
+        file_text += page.extract_text()
+    
+    return file_text
+
+def answer_query(user_input, file_text):
+    response = model.generate_content([f"Answer the following query based on the provided text, remember to STRICTLY NOT ANSWER any query that is unrelated to the provided text. Additionally, return the section of the provided text from where you found the answer. The query is - {user_input}", file_text])
+    return response.text
+
+def summarize(file_text):
+    response = model.generate_content([f"Summarize the following text - {file_text}"])
+    return response.text
+
+# for chunk in response: # generating a output stream so we dont have to wait for the entire result
+    # print(chunk.text, end="")
+
+def genai_upload(file_path):
+    file = genai.upload_file(file_path)
+    return file
+
+# print(model.count_tokens([file, '\n\nCan you summarize this file?']))
+
+def generate_response(query, file):
+    response = model.generate_content([file, f'\n\nAnswer the following query based on the provided document, remember to STRICTLY NOT ANSWER any query that is unrelated to the provided document. The query is - {query}'])
+    return response.text
